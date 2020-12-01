46bf7b72d1f93fa775f01d2acbe54a38c4f65580
Каламина Эльвира <elvira.kalamina@mail.ru>
2020-12-01 16:58:25 +0300

    fix weather service

---
+++ weatherService/src/main/java/weatherService/WeatherConfiguration.java
@@ -0,0 +1,14 @@
+package weatherService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class WeatherConfiguration {
+    @Bean
+    public RestTemplate restTesmplate() {
+        return new RestTemplate();
+    }
+
+}
--- weatherService/src/main/java/weatherService/WeatherService.java
+++ weatherService/src/main/java/weatherService/WeatherService.java
@@ -1,5 +1,6 @@
 package weatherService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import weatherService.DTO.weatherRequest.WeatherRequest;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 @Service
@@ -20,7 +22,10 @@ public class WeatherService {
     @Value("${weather.token:someToken}")
     private String token;
 
-    private static final String WEATHER_HISTORY = "http://api.weatherapi.com/v1/history.json?key=%s&q=%s&dt=$s";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String WEATHER_HISTORY = "http://api.weatherapi.com/v1/history.json?key=%s&q=%s&dt=%s";
 
     public ArrayList<WeatherResponse> getWeathersForLastNDays(int countDays, String city) throws Exception {
         ArrayList<WeatherResponse> weatherListResponse = new ArrayList<>();
@@ -31,9 +36,8 @@ public class WeatherService {
 
     private void getWeathers(int countDays, String city, ArrayList<WeatherResponse> weatherListResponse) {
         for (int i = 0; i < countDays; i++) {
-            WeatherRequest request = new RestTemplate().getForObject(getWeatherUrl(i, city), WeatherRequest.class);
+            WeatherRequest request = restTemplate.getForObject(getWeatherUrl(i, city), WeatherRequest.class);
             weatherListResponse.add(new WeatherResponse(request));
-            System.out.println(request.toString());
         }
     }
 
@@ -42,10 +46,10 @@ public class WeatherService {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
         Long callTime = System.currentTimeMillis();
         String date1 = simpleDateFormat.format(new Date(callTime - countDays * 1000 * 60 * 60 * 24));
-        return String.format(WEATHER_HISTORY, token, city, date1)
+        return String.format(WEATHER_HISTORY, token, city, date1);
     }
 
-    public void saveWeather(ArrayList<WeatherResponse> weatherListResponse) {
+    public void saveWeather(Collection<WeatherResponse> weatherListResponse) {
         weatherListResponse.forEach(weatherResponse -> {
             try {
                 weatherCrudRepository.save(new Weather(weatherResponse));
