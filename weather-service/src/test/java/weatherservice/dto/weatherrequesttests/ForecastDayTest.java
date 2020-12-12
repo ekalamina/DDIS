df02a5be21cf1d7bc2f727d1e045505377493b2e
Каламина Эльвира Андреевна <elvira.kalamina@mail.ru>
2020-12-12 22:36:33 +0300

    fix

--- weather-service/src/test/java/weatherservice/dto/weatherrequesttests/ForecastDayTest.java
+++ weather-service/src/test/java/weatherservice/dto/weatherrequesttests/ForecastDayTest.java
@@ -1,4 +1,4 @@
-package weatherservice.dto.weatherrequest;
+package weatherservice.dto.weatherrequesttests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
