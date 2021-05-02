ca724e34bb3538f61ecbdf0f793386630620eb49
Каламина Эльвира Андреевна <elvira.kalamina@mail.ru>
2021-05-02 22:04:15 +0300

    :

---
+++ currency-service/src/main/resources/db.migrations/V002__Dollar_Table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS Dollar (
+    value double precision,
+    date varchar(50)
+)
---
+++ weather-service/src/main/resources/db.migration/V002__Dollar_Table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS Weather (
+    temperature double precision,
+    date varchar(50),
+    location varchar(50)
+)
